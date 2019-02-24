127c1d0bb009f605fa726af021276a60f47159a7
陈文翰 <670509577@qq.com>
2019-02-24 17:07:49 +0800

    打包配置

--- src/main/java/com/petadoption/minprogram/MinprogramApplication.java
+++ src/main/java/com/petadoption/minprogram/MinprogramApplication.java
@@ -3,13 +3,20 @@ package com.petadoption.minprogram;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MinprogramApplication {
+public class MinprogramApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MinprogramApplication.class, args);
     }
 
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MinprogramApplication.class);
+    }
 }
 
